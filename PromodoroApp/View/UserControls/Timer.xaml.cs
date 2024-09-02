001153c5d272215e673cbc99d56486e44410c39b
Rafa <rafael.cote11@gmail.com>
2024-09-02 14:48:15 -0400

    Create Timer User Controls + Add it to Main Window

---
+++ PromodoroApp/View/UserControls/Timer.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace PromodoroApp.View.UserControls;
+
+public partial class Timer : UserControl
+{
+    public Timer()
+    {
+        InitializeComponent();
+        
+        tbTimer.Text = DateTime.Now.ToString("HH:mm");
+    }
+}
