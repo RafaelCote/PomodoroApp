54497b34fec5c82a8bc1463b3ec33b2800db886e
Rafa <rafael.cote11@gmail.com>
2024-09-05 21:17:12 -0400

    Make the time countdown 10 seconds.

--- PromodoroApp/View/UserControls/Timer.xaml.cs
+++ PromodoroApp/View/UserControls/Timer.xaml.cs
@@ -1,14 +1,28 @@
 ﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace PromodoroApp.View.UserControls;
 
 public partial class Timer : UserControl
 {
+    private DispatcherTimer _timer = null;
+    private TimeSpan _time = default;
+    
     public Timer()
     {
         InitializeComponent();
-        
-        tbTimer.Text = DateTime.Now.ToString("HH:mm");
+
+        _time = TimeSpan.FromSeconds(10);
+
+        _timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, (sender, args) =>
+        {
+            tbTimer.Text = string.Format("{0:00}:{1:00}", (int)_time.TotalMinutes, _time.Seconds);
+            if (_time == TimeSpan.Zero)
+                _timer.Stop();
+            _time = _time.Add(TimeSpan.FromSeconds(-1));
+
+        }, Application.Current.Dispatcher);
     }
 }
