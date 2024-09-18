8cc878e4b9d4e83cab548c9571b686e9297caffa
Rafa <rafael.cote11@gmail.com>
2024-09-18 18:17:49 -0400

    Beginning styling the app.

---
+++ PromodoroApp/Classes/RoundedButton.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace PromodoroApp.Classes;
+
+public partial class RoundedButton : UserControl
+{
+    public RoundedButton()
+    {
+        InitializeComponent();
+    }
+}
